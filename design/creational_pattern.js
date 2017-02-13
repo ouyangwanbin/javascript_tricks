d2681c4a9a2217885635c6359462d9504761d375
Wanbin ouyang <wanbinouyang@Wanbins-MacBook-Pro.local>
2017-02-12 21:41:11 -0800

    add creational_pattern

---
+++ design/creational_pattern.js
@@ -0,0 +1,11 @@
+var Person = function( firstName, lastName ){
+	this.firstName = firstName;
+	this.lastName = lastName;
+}
+
+Person.prototype.sayName = function( ){
+	console.log("My name is " + this.firstName + " " + this.lastName );
+}
+
+var p1 = new Person("Wanbin", "Ouyang");
+p1.sayName();
